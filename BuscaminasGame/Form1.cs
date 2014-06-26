c060112519bab5223762bd3ae6aae72cb501a5d8
L3V147H4N <gamatusevich@gmail.com>
2014-06-26 13:34:10 -0300

    gui

--- BuscaminasGame/Form1.cs
+++ BuscaminasGame/Form1.cs
@@ -11,9 +11,82 @@ namespace BuscaminasGame
 {
     public partial class Form1 : Form
     {
+        private int boardSize;
+        private Button[,] grid;
+
+        private int runs = 0;
+
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();            
+
+            int x = 40;
+            int y = 40;
+
+            this.boardSize = 20;
+            this.grid = new Button[boardSize, boardSize];
+
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+
+                    //Create new Button
+                    System.Windows.Forms.Button spot = new System.Windows.Forms.Button();
+
+                    //Set Properties
+                    spot.Location = new System.Drawing.Point(x, y);
+
+                    spot.Name = "Spot[" + i + "," + j + "]";
+                    spot.Size = new System.Drawing.Size(24, 24);
+                    spot.TabIndex = 0;
+                    spot.Text = "";
+                    spot.UseVisualStyleBackColor = true;
+
+                    //Add Button
+                    this.Controls.Add(spot);
+
+                    spot.Click += new System.EventHandler(this.Button_Click_Spot);
+
+                    this.grid[i, j] = spot;
+
+                    x += 22;
+
+                }
+
+                y += 22;
+                x = 40;
+            }
+
         }
+
+        private void Button_Click_Spot(object sender, EventArgs e) 
+        {
+
+            Button thisButton = (Button)sender;
+
+            thisButton.Enabled = false;
+
+            this.RedrawBoard();
+
+        }
+
+        private void RedrawBoard() 
+        {
+            for (int i = 0; i < this.boardSize; i++)
+            {
+
+                for (int j = 0; j < this.boardSize; j++)
+                {
+                    
+                    this.grid[i, j].Text = this.runs.ToString();
+                    
+
+                }
+
+            }
+
+        }
+
     }
 }
